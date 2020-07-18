2354a70e6a0351c1c44462386732ae64f47da9a1
Mizok <karasunohina@gmail.com>
2020-07-19 01:08:06 +0800

    update

--- src/js/main.js
+++ src/js/main.js
@@ -3,21 +3,118 @@ import p5 from 'p5';
 
 
 function initializePage() {
+  let conf = {};
+  let ballGroupRef = {};
+  let randomColor = function () {
+    var color = 'rgb(' + parseInt(Math.random() * 255) + ',' + parseInt(Math.random() * 255) + ',' + parseInt(Math.random() * 255) + ')'
+    return color;
+  }
   let sketch = function (p) {
 
     p.setup = function () {
       p.createCanvas(p.windowWidth, p.windowHeight);
+      p.background(0);
+
+      conf.gravity = 0.098;
+      conf.repulsiveX = 0.1;
+      conf.timeOut = 200;
+      conf.maxSize = 100;
+      conf.maxSpeedX = 10;
+      conf.maxSpeedY = 10;
+
+      ballGroupRef = {
+        ballArray: [],
+        genBall: function (x, y, radius, bgColor, timer) {
+          var ball = {
+            x: x,
+            y: y,
+            radius: radius,
+            bgColor: bgColor,
+            speedX: Math.random() * conf.maxSpeedX,
+            speedY: Math.random() * conf.maxSpeedY,
+            timer: timer
+          }
+
+          if (this.ballArray.length > 300) {
+            var overflow = confirm('住手！！～～～～再丟球視窗就要爆炸啦~~~~~')
+            if (overflow) {
+              var overflowConfrim = confirm('再不清空就～要～爆～啦～～～～準備清空啦');
+              if (overflowConfrim) {
+                p.mouseIsPressed = false;
+                this.ballArray = [];
+              }
+            }
+          }
+          else {
+            this.ballArray.push(ball);
+          }
+
+        },
+        defaultGen: function () {
+          var num = 20;
+          for (let i = 0; i < num; i++) {
+            this.genBall(Math.random() * p.windowWidth, Math.random() * p.windowHeight, Math.random() * conf.maxSize, randomColor(), conf.timeOut)
+          }
+        },
+
+      };
 
     };
 
     p.draw = function () {
-      p.background(0, 0, 0, 0.75);
+      p.background(0, 90);
+      p.noStroke();
 
-      p.stroke(255, 255, 255);
-      p.line(0, 0, p.mouseX, p.mouseY);
-      p.ellipse(p.mouseX, p.mouseY, 200, 200);
-      p.fill(p.mouseX * 0.1, p.mouseY * 0.2, p.mouseY * 0.5);
+
+      if (p.mouseIsPressed) {
+        ballGroupRef.genBall(p.mouseX, p.mouseY, Math.random() * conf.maxSize, randomColor(), 0);
+
+
+      }
+      for (let i = 0; i < ballGroupRef.ballArray.length; i++) {
+
+        //change balls speed at next frame;
+
+
+        if (ballGroupRef.ballArray[i].timer > conf.timeOut) {
+          ballGroupRef.ballArray[i].speedX -= conf.repulsiveX;
+          ballGroupRef.ballArray[i].speedY += conf.gravity;
+
+
+          //change balls position at next frame;
+          if (ballGroupRef.ballArray[i].x > p.windowWidth || ballGroupRef.ballArray[i].x < 0) {
+            ballGroupRef.ballArray[i].speedX = - ballGroupRef.ballArray[i].speedX;
+          }
+
+
+          if (ballGroupRef.ballArray[i].y > p.windowHeight || ballGroupRef.ballArray[i].y < 0) {
+            ballGroupRef.ballArray[i].speedY = - ballGroupRef.ballArray[i].speedY;
+          }
+
+
+          ballGroupRef.ballArray[i].x += ballGroupRef.ballArray[i].speedX;
+          ballGroupRef.ballArray[i].y += ballGroupRef.ballArray[i].speedY;
+
+        }
+        else {
+          ballGroupRef.ballArray[i].timer += 3;
+        }
+
+
+
+
+        //draw ball
+        if (ballGroupRef.ballArray.length) {
+          p.fill(ballGroupRef.ballArray[i].bgColor);
+          p.ellipse(ballGroupRef.ballArray[i].x, ballGroupRef.ballArray[i].y, ballGroupRef.ballArray[i].radius, ballGroupRef.ballArray[i].radius);
+
+
+        }
+
+      }
     };
+
+
 
     window.addEventListener('resize', debounce(function () {
       p.resizeCanvas(p.windowWidth, p.windowHeight);
@@ -40,5 +137,12 @@ function main() {
 }
 
 main();
+
+
+
+
+
+
+
 
 
